fb034c9d7eb74966012cce1dbe169ed7e2dc784a
MH Emon <mhemon@MHs-MacBook-Air.local>
2025-12-11 12:40:01 +0600

    Blogs Section done

--- components/Blog.jsx
+++ components/Blog.jsx
@@ -14,7 +14,7 @@ const BLOG_POSTS = [
       "In today digital landscape, responsive web design is not just a trend; it is a necessity. Explore the principles and...",
     date: "June 4, 2020",
     tags: ["HTML", "CSS","Tailwind","Javascript"],
-    image: "/blog-post-1.jpg", // Replace with your image path
+    image: "/blog-1.jpg", // Replace with your image path
     link: "https://example.com/responsive-design",
   },
   {
@@ -24,7 +24,7 @@ const BLOG_POSTS = [
       "Discover innovative ways to create captivating slideshows that engage your audience. From stunning visuals to...",
     date: "may 21, 2022",
     tags: ["HTML", "CSS","Tailwind","React"],
-    image: "/blog-post-2.jpg",
+    image: "/blog-2.jpg",
     link: "https://example.com/creative-slideshows",
   },
   {
@@ -34,7 +34,7 @@ const BLOG_POSTS = [
       "Explore the benefits of incorporating image gallery lightboxes into your website design. From improved user...",
     date: "June 43, 2022",
     tags: ["HTML", "CSS","Nextjs","React"],
-    image: "/blog-post-3.jpg",
+    image: "/blogs-3.jpg",
     link: "https://example.com/image-lightboxes",
     save: true, // Matching the 'Save' pin in the screenshot
   },
